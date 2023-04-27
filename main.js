f2f95ffc80c8f8f2f8d55564f696510a9180cae0
Josué <ajosue1994@gmail.com>
2023-04-27 16:16:18 -0500

    firtsUP

---
+++ main.js
@@ -0,0 +1,35 @@
+async function postData(data) {
+    const response = await fetch(`verificar.php`, {
+        method: "POST",
+        body: data,
+    }).then((res) => res.text());
+    return await response;
+}
+
+/* $(document).on("submit", "#frmExcelImport", async (e) => {
+    e.preventDefault();
+    let form = document.querySelector("#frmExcelImport");
+    let datos = new FormData(form);
+    //datos.append("accion", "LOGIN");
+    let respuesta = await postData(datos);
+    if (respuesta === "OK") window.location.assign("index.php");
+    else
+        Swal.fire({
+            icon: 'error',
+            title: 'Error de Acceso',
+            text: 'Datos Incorrectos',
+            confirmButtonColor: '#3085d6',
+        })
+console.log(respuesta)
+});
+ */
+const form = document.querySelector('#frmExcelImport');
+const contTable = document.querySelector('#cont-table');
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    let frm = document.querySelector("#frmExcelImport");
+    let datos = new FormData(frm);
+    let respuesta = await postData(datos);
+    contTable.innerHTML = respuesta;
+})
